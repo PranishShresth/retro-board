5bbeab8c1bd37c7112a62017428da7a11e1e472a
PranishShresth <yunche64@gmail.com>
2021-10-08 22:21:37 +1000

    fixing up environment variable assertion

--- config/db.ts
+++ config/db.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 async function connectToDB(dbString: string) {
   try {
     await mongoose.connect(dbString);
+    console.log("DB connected");
   } catch (error) {
     console.log(error);
   }
--- index.ts
+++ index.ts
@@ -1,16 +1,20 @@
 import express from "express";
 import { config } from "dotenv";
 import connectToDB from "./config/db";
+import ApiRoutes from "./routes";
+
 const app = express();
 const PORT = process.env.PORT || 5000;
-const DB_STRING = process.env.MONGO_URI;
 config();
+
+const DB_STRING = process.env.MONGO_URI!;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-connectToDB(DB_STRING);
+app.use("/api/v1", ApiRoutes);
 
 app.listen(PORT, () => {
   console.log(`Listening to ${PORT}`);
 });
+
+connectToDB(DB_STRING);
