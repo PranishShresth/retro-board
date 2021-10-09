2a162818295954db39ffc7827fbf46c8c9fcecc0
PranishShresth <yunche64@gmail.com>
2021-10-09 13:12:02 +1000

    adding axios as http client

---
+++ client/src/saga/rootSaga.ts
@@ -0,0 +1,14 @@
+import { put, takeEvery } from "redux-saga/effects";
+
+// ...
+
+// Our worker Saga: will perform the async increment task
+export function* incrementAsync() {
+  yield delay(1000);
+  yield put({ type: "INCREMENT" });
+}
+
+// Our watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
+export function* watchRootSaga() {
+  yield takeEvery("INCREMENT_ASYNC", incrementAsync);
+}
