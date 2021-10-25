d4b796bdf90bcda6ebb78d026814759661a026ed
PranishShresth <yunche64@gmail.com>
2021-10-25 22:07:31 +1000

    update/delete list controller

--- controller/list.ts
+++ controller/list.ts
@@ -33,3 +33,46 @@ export const addListToBoard = async (req: AddListRequest, res: Response) => {
     res.status(500).send("Internal Server Error");
   }
 };
+
+interface DeleteListRequest {
+  params: {
+    list_id: string;
+  };
+}
+export const deleteList = async (req: DeleteListRequest, res: Response) => {
+  const { list_id } = req.params;
+  try {
+    await List.findByIdAndDelete(list_id);
+    res.status(200).send({ success: true });
+  } catch (err) {
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+interface UpdateListRequest {
+  params: {
+    list_id: string;
+  };
+  body: {
+    list_title: string;
+  };
+}
+export const updateList = async (req: UpdateListRequest, res: Response) => {
+  const { list_title } = req.body;
+  const { list_id } = req.params;
+
+  try {
+    const updatedList = await List.findByIdAndUpdate(
+      list_id,
+      {
+        $set: {
+          list_title: list_title,
+        },
+      },
+      { new: true }
+    );
+    res.status(200).send(updatedList);
+  } catch (err) {
+    res.status(500).send("Internal Server Error");
+  }
+};
