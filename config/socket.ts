89c94c0a5a2f7c8d17d89fd79f7c89728f5b9882
PranishShresth <yunche64@gmail.com>
2021-10-25 21:16:08 +1000

    merge main to master

--- config/socket.ts
+++ config/socket.ts
@@ -1,11 +1,13 @@
 import { Server, Socket } from "socket.io";
 import { Server as HttpServer } from "http";
 import { Express } from "express";
+import { config } from "dotenv";
+config();
 
 function socketIO(server: HttpServer, app: Express) {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: process.env.CLIENT_URL,
       methods: ["GET", "POST", "PUT", "DELETE"],
     },
   });
